ab493396964bdec8f97e0b46af01b64eedd5139d
VVeljovic <veljko.veljovic13@gmail.com>
2025-04-05 20:16:52 +0200

    First two problems

---
+++ Neetcode/Neetcode/Arrays & Hashing/Arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neetcode.Arrays___Hashing
+{
+    public sealed class Arrays
+    {
+
+        public bool HasDuplicate(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dictionary.TryAdd(nums[i], i))
+                {
+
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+}
---
+++ Neetcode/Neetcode/Program.cs
@@ -0,0 +1,34 @@
+﻿
+var s = "racecar"; var t = "carrace";
+
+Console.WriteLine(IsAnagram(s,t));
+bool IsAnagram(string s, string t)
+{
+    var dictionary = new Dictionary<char, int>();
+    foreach(var character in s)
+    {
+        if (dictionary.TryAdd(character, 1))
+        {
+
+        }
+        else
+        {
+            dictionary[character] = dictionary[character] + 1;
+        }
+    }
+
+    for (int i =0; i< t.Length; i ++)
+    {
+        var slovo = t[i];
+        var oldLen = t.Length;
+        t = new string(t.Where(x => x != slovo).ToArray());
+        var newLen = t.Length;
+
+        if (dictionary[slovo] != oldLen - newLen)
+        {
+            return false;
+        }
+    }
+    return true;
+
+}
