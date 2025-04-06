accdaf49f8baae0facc40e875dd7fecfce6ed7b8
VVeljovic <veljko.veljovic13@gmail.com>
2025-04-06 16:16:21 +0200

    Third problem

--- Neetcode/Neetcode/Arrays & Hashing/Arrays.cs
+++ Neetcode/Neetcode/Arrays & Hashing/Arrays.cs
@@ -27,6 +27,66 @@ namespace Neetcode.Arrays___Hashing
             return false;
         }
 
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            var dictionary = new Dictionary<char, int>();
+            foreach (var character in s)
+            {
+                if (dictionary.TryAdd(character, 1))
+                {
+
+                }
+                else
+                {
+                    dictionary[character] = dictionary[character] + 1;
+                }
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                var slovo = t[i];
+                var oldLen = t.Length;
+                t = new string(t.Where(x => x != slovo).ToArray());
+                var newLen = t.Length;
+
+                if (dictionary.TryGetValue(slovo, out var value))
+                {
+
+                    if (value != oldLen - newLen)
+                        return false;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dictionary.TryGetValue(target - nums[i], out var index))
+                {
+                    return new int[] {index, i};
+                }
+
+                else
+                {
+                    dictionary.TryAdd(nums[i], i);
+                }
+            }
+
+            return null;
+        }
 
     }
 }
